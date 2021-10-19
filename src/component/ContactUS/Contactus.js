4160a3449640eadbc33e1687e7d6d92edd488984
amirul-islam <amirulislam.shanto.75@gmail.com>
2021-10-19 13:58:32 +0600

    Second commit

--- src/component/ContactUS/Contactus.js
+++ src/component/ContactUS/Contactus.js
@@ -8,23 +8,26 @@ const Contactus = () => {
         <h1>Contact Us</h1>
       </div>
       <div className="row py-4">
-        <div className="col-12 col-md-6 col-lg-4">
+        <div className="col-12 col-md-6 mb-4 col-lg-4">
           <h3>Headquarters</h3>
           <h5>#42 PurboPara</h5>
           <h5>Gopalnagar Bazar</h5>
           <h5>FaridPur,Pabna</h5>
+          <hr className="w-75 mx-auto" />
         </div>
-        <div className="col-12 col-md-6 col-lg-4">
+        <div className="col-12 col-md-6 mb-4 col-lg-4">
           <h3>General Contact</h3>
           <h5>Toll Free: 888-666-1205</h5>
           <h5>Phone: 732-951-145</h5>
           <h5>Fax: 485-102-34 </h5>
+          <hr className="w-75 mx-auto" />
         </div>
-        <div className="col-12 col-md-6 col-lg-4">
+        <div className="col-12 col-md-6 mb-4 col-lg-4">
           <h3>Outdoor an Emergency</h3>
           <h5>Outdoor: 738-666-1205</h5>
           <h5>Ambulance: 452-951-145</h5>
           <h5>Any-Objection: 999</h5>
+          <hr className="w-75 mx-auto" />
         </div>
       </div>
     </div>
